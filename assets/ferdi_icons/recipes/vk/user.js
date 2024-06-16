caebe29dc119e9d6a26a32a5f210f65bb59a33a5
haykh <haykh.astro@gmail.com>
2024-06-16 14:26:26 -0400

    posh

--- assets/ferdi_icons/recipes/vk/user.js
+++
@@ -1,9 +0,0 @@
-module.exports = (config, Ferdium) => {
-  // Write your scripts here
-  window.onload = function () {
-    if (window.location.href == "https://vk.com/feed") {
-      window.location.replace('https://vk.com/im');
-    }
-  }
-  console.log("Hello, World!", config);
-};
