1a169796f0a29c5634b5019902f25635f5c27799
hayk <haykh.astro@gmail.com>
2025-04-28 02:06:53 -0400

    minrc upd + vim

--- scripts/minrc.sh
+++ scripts/minrc.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/env bash
 
-HOMEDIR=$(mktemp -d)
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+
+printf "${BLUE}Master directory${NC} "
+read -p "(default: $HOME): " HOMEDIR
+HOMEDIR=${HOMEDIR:-$HOME}
+
+printf "Master directory is ${RED}${HOMEDIR}${NC}\n"
 
 TEMPDIR=$HOMEDIR/.temp
 LOCAL=$HOMEDIR/.local
 ZSHFUNC=$HOMEDIR/.zfunc
-PYTHONEXEC=$(which python3.11)
+PYTHONEXEC=/usr/licensed/anaconda3/2024.10/bin/python3
 OPTPATH=$HOMEDIR/opt
+
+EZA_VERSION=0.21.0
+FZF_VERSION=0.61.1
+BAT_VERSION=0.25.0
+STARSHIP_VERSION=1.22.1
+PIPX_VERSION=1.7.1
+NVIM_VERSION=0.11.1
+GO_VERSION=1.24.2
 
 mkdir -p $TEMPDIR && cd $TEMPDIR
 mkdir -p $LOCAL/bin
@@ -14,11 +30,13 @@ mkdir -p $ZSHFUNC
 mkdir -p $OPTPATH
 
 function prompt() {
-  local prompt="$1"
+  local prompt="\033[0;34m$1\033[0m"
   local default="$2"
   local response
 
-  read -p "$prompt ($default): " response
+  printf "\n${BLUE}${prompt}${NC}: "
+
+  read -p "" response
   response=${response:-$default}
 
   if [[ "$response" =~ ^[Yy]$ ]]; then
@@ -29,125 +47,181 @@ function prompt() {
 }
 
 # install eza
-prompt "install eza? (Y/n)" "y"
+prompt "install eza v${EZA_VERSION} in $LOCAL/bin? (Y/n)" "y"
 if [[ $? -ne 0 ]]; then
   echo "eza installation skipped"
-else 
+else
   echo "installing eza..."
-  wget https://github.com/eza-community/eza/releases/download/v0.21.0/eza_x86_64-unknown-linux-gnu.tar.gz && \
-    tar xvf eza_x86_64-unknown-linux-gnu.tar.gz && \
-    mv eza $LOCAL/bin/ && \
-    rm eza_x86_64-unknown-linux-gnu.tar.gz && \
-    echo "eza installed to $LOCAL/bin" || \
+  wget https://github.com/eza-community/eza/releases/download/v${EZA_VERSION}/eza_x86_64-unknown-linux-gnu.tar.gz &&
+    tar xvf eza_x86_64-unknown-linux-gnu.tar.gz &&
+    mv eza $LOCAL/bin/ &&
+    rm eza_x86_64-unknown-linux-gnu.tar.gz &&
+    echo "eza installed to $LOCAL/bin" ||
     echo "eza installation failed"
 fi
 
-
 # install fzf
-prompt "install fzf? (Y/n)" "y"
+prompt "install fzf v${FZF_VERSION} in $LOCAL/bin? (Y/n)" "y"
 if [[ $? -ne 0 ]]; then
   echo "fzf installation skipped"
 else
   echo "installing fzf..."
-  wget https://github.com/junegunn/fzf/releases/download/v0.61.1/fzf-0.61.1-linux_amd64.tar.gz && \
-    tar xvf fzf-0.61.1-linux_amd64.tar.gz && \
-    mv fzf $LOCAL/bin/ && \
-    rm fzf-0.61.1-linux_amd64.tar.gz && \
-    echo "fzf installed to $LOCAL/bin" || \
+  wget https://github.com/junegunn/fzf/releases/download/v${FZF_VERSION}/fzf-${FZF_VERSION}-linux_amd64.tar.gz &&
+    tar xvf fzf-${FZF_VERSION}-linux_amd64.tar.gz &&
+    mv fzf $LOCAL/bin/ &&
+    rm fzf-${FZF_VERSION}-linux_amd64.tar.gz &&
+    echo "fzf installed to $LOCAL/bin" ||
     echo "fzf installation failed"
 fi
 
 # install bat
-prompt "install bat? (Y/n)" "y"
+prompt "install bat v${BAT_VERSION} in $LOCAL/bin? (Y/n)" "y"
 if [[ $? -ne 0 ]]; then
   echo "bat installation skipped"
 else
   echo "installing bat..."
-  wget https://github.com/sharkdp/bat/releases/download/v0.25.0/bat-v0.25.0-x86_64-unknown-linux-gnu.tar.gz && \
-    tar xvf bat-v0.25.0-x86_64-unknown-linux-gnu.tar.gz && \
-    mv bat-v0.25.0-x86_64-unknown-linux-gnu/bat $LOCAL/bin/ && \
-    mv bat-v0.25.0-x86_64-unknown-linux-gnu/autocomplete/bat.zsh $ZSHFUNC && \
-    rm -rf bat-v0.25.0-x86_64-unknown-linux-gnu && \
-    echo "bat installed to $LOCAL/bin" || \
+  wget https://github.com/sharkdp/bat/releases/download/v${BAT_VERSION}/bat-v${BAT_VERSION}-x86_64-unknown-linux-gnu.tar.gz &&
+    tar xvf bat-v${BAT_VERSION}-x86_64-unknown-linux-gnu.tar.gz &&
+    mv bat-v${BAT_VERSION}-x86_64-unknown-linux-gnu/bat $LOCAL/bin/ &&
+    mv bat-v${BAT_VERSION}-x86_64-unknown-linux-gnu/autocomplete/bat.zsh $ZSHFUNC &&
+    rm -rf bat-v${BAT_VERSION}-x86_64-unknown-linux-gnu &&
+    echo "bat installed to $LOCAL/bin" ||
     echo "bat installation failed"
 fi
 
 # install starship
-prompt "install starship? (Y/n)" "y"
+prompt "install starship v${STARSHIP_VERSION} in $LOCAL/bin? (Y/n)" "y"
 if [[ $? -ne 0 ]]; then
   echo "starship installation skipped"
-else 
+else
   echo "installing starship..."
-  wget https://github.com/starship/starship/releases/download/v1.22.1/starship-x86_64-unknown-linux-gnu.tar.gz && \
-    tar xvf starship-x86_64-unknown-linux-gnu.tar.gz && \
-    mv starship $LOCAL/bin/ && \
-    rm starship-x86_64-unknown-linux-gnu.tar.gz && \
-    echo "starship installed to $LOCAL/bin" || \
+  wget https://github.com/starship/starship/releases/download/v${STARSHIP_VERSION}/starship-x86_64-unknown-linux-gnu.tar.gz &&
+    tar xvf starship-x86_64-unknown-linux-gnu.tar.gz &&
+    mv starship $LOCAL/bin/ &&
+    rm starship-x86_64-unknown-linux-gnu.tar.gz &&
+    echo "starship installed to $LOCAL/bin" ||
     echo "starship installation failed"
-fi 
+fi
 
 # install omz & plugins
-prompt "install oh-my-zsh? (Y/n)" "y"
+prompt "install oh-my-zsh + plugins $HOMEDIR/.oh-my-zsh? (Y/n)" "y"
 if [[ $? -ne 0 ]]; then
-  echo "oh-my-zsh installation skipped"
+  echo "oh-my-zsh + plugins installation skipped"
 else
-  echo "installing oh-my-zsh..."
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended && \
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOMEDIR/.oh-my-zsh/custom}/plugins/zsh-autosuggestions && \
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOMEDIR/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting && \
-    echo "oh-my-zsh and plugins installed" || \
-    echo "oh-my-zsh and plugins installation failed"
+  echo "installing oh-my-zsh + plugins..."
+  ZSH="${HOMEDIR}/.oh-my-zsh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended &&
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOMEDIR/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOMEDIR/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&
+    echo "oh-my-zsh & plugins installed" ||
+    echo "oh-my-zsh & plugins installation failed"
 fi
 
 # install pipx
-prompt "install pipx? (Y/n)" "y"
+prompt "install pipx v${PIPX_VERSION} in $LOCAL/bin? (Y/n)" "y"
 if [[ $? -ne 0 ]]; then
   echo "pipx installation skipped"
 else
   echo "installing pipx..."
-  wget https://github.com/pypa/pipx/releases/download/1.7.1/pipx.pyz && \
-    mv pipx.pyz $OPTPATH/ && \
-    chmod +x $OPTPATH/pipx.pyz && \
-    echo "pipx installed to $OPTPATH" || \
+  wget https://github.com/pypa/pipx/releases/download/${PIPX_VERSION}/pipx.pyz &&
+    mv pipx.pyz $LOCAL/bin &&
+    chmod +x $LOCAL/bin/pipx.pyz &&
+    echo "pipx installed to $LOCAL/bin" ||
     echo "pipx installation failed"
 fi
 
-# install fortls
-prompt "install fortls with pipx? (Y/n)" "y"
+# install fortls + clang-format + cmakelang
+prompt "install fortls + clang-format + cmakelang with pipx? (Y/n)" "y"
 if [[ $? -ne 0 ]]; then
-  echo "fortls installation skipped"
+  echo "fortls + clang-format + cmakelang installation skipped"
 else
-  echo "installing fortls..."
-  $PYTHONEXEC $OPTPATH/pipx.pyz install fortls && \
-    echo "fortls installed" || \
+  echo "installing fortls + clang-format + cmakelang..."
+  $PYTHONEXEC $LOCAL/bin/pipx.pyz install fortls &&
+    echo "fortls installed" ||
     echo "fortls installation failed"
+  $PYTHONEXEC $LOCAL/bin/pipx.pyz install clang-format &&
+    echo "clang-format installed" ||
+    echo "clang-format installation failed"
+  $PYTHONEXEC $LOCAL/bin/pipx.pyz install cmakelang &&
+    echo "cmakelang installed" ||
+    echo "cmakelang installation failed"
 fi
 
 # install nvim
-prompt "install nvim? (Y/n)" "y"
+prompt "install nvim v${NVIM_VERSION} in ${OPTPATH}/nvim-linux-x86-64? (Y/n)" "y"
 if [[ $? -ne 0 ]]; then
   echo "nvim installation skipped"
 else
   echo "installing nvim..."
-  wget https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-linux-x86_64.tar.gz && \
-    tar xvf nvim-linux-x86_64.tar.gz && \
-    mv nvim-linux-x86_64 $OPTPATH && \
-    ln -s $OPTPATH/nvim-linux-x86_64/bin/nvim $LOCAL/bin/ && \
-    rm nvim-linux-x86_64.tar.gz && \
-    echo "nvim installed to $LOCAL/bin" || \
+  wget https://github.com/neovim/neovim-releases/releases/download/v${NVIM_VERSION}/nvim-linux-x86_64.tar.gz &&
+    tar xvf nvim-linux-x86_64.tar.gz &&
+    mv nvim-linux-x86_64 $OPTPATH &&
+    ln -s $OPTPATH/nvim-linux-x86_64/bin/nvim $LOCAL/bin/ &&
+    rm nvim-linux-x86_64.tar.gz &&
+    echo "nvim installed to $LOCAL/bin" ||
     echo "nvim installation failed"
 fi
 
 # install n
-prompt "install n? (Y/n)" "y"
+prompt "install n in $LOCAL/n? (Y/n)" "y"
 if [[ $? -ne 0 ]]; then
   echo "n installation skipped"
 else
   echo "installing n..."
-  export N_PREFIX=$LOCAL/n && \
-    curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | bash -s install lts && \
-    echo "n installed to $LOCAL/n" || \
+  export N_PREFIX=$LOCAL/n &&
+    curl -fsSL https://raw.githubusercontent.com/tj/n/master/bin/n | bash -s install lts &&
+    echo "n installed to $LOCAL/n" ||
     echo "n installation failed"
+fi
+
+# install go
+prompt "install go v${GO_VERSION} in $LOCAL/go? (Y/n)" "y"
+if [[ $? -ne 0 ]]; then
+  echo "go installation skipped"
+else
+  echo "installing go..."
+  wget https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz &&
+    tar -C "$LOCAL" -xzf go${GO_VERSION}.linux-amd64.tar.gz &&
+    echo "go installed to $LOCAL/go" ||
+    echo "go installation failed"
+fi
+
+# install gopls
+prompt "install gopls with go? (Y/n)" "y"
+if [[ $? -ne 0 ]]; then
+  echo "gopls installation skipped"
+else
+  echo "installing gopls..."
+  go install golang.org/x/tools/gopls@latest &&
+    echo "gopls installed" ||
+    echo "gopls installation failed"
+fi
+
+# install rust + cargo
+prompt "install rust + cargo in $HOMEDIR/.rustup & $HOMEDIR/.cargo? (Y/n)" "y"
+if [[ $? -ne 0 ]]; then
+  echo "rust + cargo installation skipped"
+else
+  echo "installing rust + cargo..."
+  CARGO_HOME="$HOMEDIR/.cargo" RUSTUP_HOME="$HOMEDIR/.rustup" bash -c 'curl https://sh.rustup.rs -sSf | sh -s -- -y' &&
+    echo "rust & cargo installed in $HOMEDIR/.rustup and $HOMEDIR/.cargo" ||
+    echo "rust & cargo installation failed"
+fi
+
+# install stylua + neocmakelsp + ripgrep
+prompt "install stylua + neocmakelsp + ripgrep with cargo? (Y/n)" "y"
+if [[ $? -ne 0 ]]; then
+  echo "stylua + neocmakelsp + ripgrep installation skipped"
+else
+  echo "installing stylua + neocmakelsp + ripgrep..."
+  $HOMEDIR/.cargo/bin/cargo install stylua &&
+    echo "stylua installed" ||
+    echo "stylua installation failed"
+  $HOMEDIR/.cargo/bin/cargo install neocmakelsp &&
+    echo "neocmakelsp installed" ||
+    echo "neocmakelsp installation failed"
+  $HOMEDIR/.cargo/bin/cargo install ripgrep &&
+    echo "ripgrep installed" ||
+    echo "ripgrep installation failed"
 fi
 
 # rewrite zshrc
@@ -156,16 +230,19 @@ if [[ $? -ne 0 ]]; then
   echo ".zshrc rewrite skipped"
 else
   echo "rewriting .zshrc..."
-  zshrc_content=$(cat <<EOF
+  zshrc_content=$(
+    cat <<EOF
 export TERM=xterm-256color
 export PATH=\$PATH:$LOCAL/bin
-export PATH=\$PATH:\$HOME/.n/bin
-export ZSH="\$HOME/.oh-my-zsh"
-export N_PREFIX="\$HOME/.local/n"
+export PATH=\$PATH:$LOCAL/go/bin
+export PATH=\$PATH:$LOCAL/n/bin
+export PATH=\$PATH:$HOMEDIR/.cargo/bin
+export ZSH="$HOMEDIR/.oh-my-zsh"
+export N_PREFIX="$LOCAL/n"
 
 plugins=(git colorize colored-man-pages extract zsh-autosuggestions zsh-syntax-highlighting)
 
-source \$ZSH/oh-my-zsh.sh
+source $HOMEDIR/.oh-my-zsh/oh-my-zsh.sh
 
 eval "\$(starship init zsh)"
 eval "\$(fzf --zsh)"
@@ -182,15 +259,15 @@ alias -- vi=nvim
 alias -- vim=nvim
 export EDITOR=nvim
 
-fpath=(\$HOME/.zfunc \$fpath)
+fpath=($HOMEDIR/.zfunc \$fpath)
 autoload compinit -Uz && compinit
 
 export PYTHON=$PYTHONEXEC
-alias -- pipx="\$PYTHON $OPTPATH/pipx.pyz"
+alias -- pipx="$PYTHONEXEC $LOCAL/bin/pipx.pyz"
 
 alias -- sque="squeue --start -u \$USER"
 EOF
-)
+  )
 
-  echo "$zshrc_content" > $HOMEDIR/.zshrc
-fi 
+  echo "$zshrc_content" >$HOMEDIR/.zshrc
+fi
